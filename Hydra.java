b9e695df614ea09364ea32f1c9c661ecf8d9c3f6
BenTibnam <BenTibnam@users.noreply.github.com>
2018-03-14 14:18:55 -0400

    Hydra.java uploaded

---
+++ Hydra.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+* Despite what the text says, this is not a real virus, all it does is
+* create new windows of itself when the user tries to close the first
+* window, but it is a good program to annoy your friends.
+* */
+
+public class Hydra {
+    private JFrame mainFrame;
+    private JLabel warning;
+
+    public void makeWindow(){
+        this.mainFrame = new JFrame("Hydra Virus 1.0.0");
+        this.warning = new JLabel("", JLabel.CENTER);
+
+        this.warning.setText("This is the Hydra Virus, cut one head off, two more grow.");
+        this.mainFrame.add(warning);
+
+        this.mainFrame.setSize(500, 100);
+
+        this.mainFrame.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent we){
+                makeWindow();
+                makeWindow();
+            }
+        });
+
+        this.mainFrame.setVisible(true);
+        mainFrame.setLocation((int)(Math.random() * 1920), (int)(Math.random() * 1080));
+    }
+
+    public Hydra() {
+        this.makeWindow();
+    }
+
+    public static void main(String args[]){
+        Hydra first = new Hydra();
+
+    }
+}
